17214615d06038589fb206cf82ee5805876f7767
chenmengyuan <a1090412761@qq.com>
2020-08-30 20:10:12 +0800

    xiugai

---
+++ js/lunbo1.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    effect : 'fade',
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
